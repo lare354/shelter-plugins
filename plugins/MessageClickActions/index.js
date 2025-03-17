90ef492e295a903656d78c85287987e46448708a
lare354 <lare354@duck.com>
2025-03-17 06:27:38 +0100

    nvm

--- plugins/MessageClickActions/index.js
+++ plugins/MessageClickActions/index.js
@@ -136,7 +136,7 @@ function MCA(e) {
             );
         }
         
-        else if ( !editMessageStore.isEditing(getCurrentChannel(), message.id)){
+        else if ( !EditMessageStore.isEditing(channelId, message.id)){
             dispatcher.dispatch({
                 type: "MESSAGE_START_EDIT",
                 channelId: channelId,
